c787810394d9e092f8ce472f1716eb5e137c3cd0
Ozankrd <151177841+Ozankrd@users.noreply.github.com>
2023-11-25 19:56:42 +0100

    Add files via upload

---
+++ niveau 3 finale/niveau_3.c
@@ -0,0 +1,16 @@
+#include"niveau 3.h"
+
+#include <stdio.h>
+#include <string.h>
+#include <conio.h>
+#include <windows.h>
+#include <time.h>
+
+int main() {
+    int lignes = 10;
+    int colonnes = 20;
+    int lignestemps=30, colonnestemps=30;
+    printf("Debut du programme \n");
+    niveau_3();
+    return 0;
+}
